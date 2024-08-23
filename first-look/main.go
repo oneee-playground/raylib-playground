dae39b2dd75fd786c273b5f832eea21477127406
onee-only <kimww0306@gmail.com>
2024-08-23 13:57:31 +0900

    first-look: Make penguin move

--- first-look/main.go
+++ first-look/main.go
@@ -2,18 +2,93 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
+var (
+	penguinSprite rl.Texture2D
+
+	spriteRect rl.Rectangle
+	playerRec  rl.Rectangle
+
+	spriteFaceDir = faceDown
+	spriteSeq     = 0
+	keyDownCnt    = 0
+)
+
+const (
+	faceUp float32 = iota
+	faceRight
+	faceDown
+	faceLeft
+)
+
+const (
+	spriteHeight float32 = 32
+	spriteWidth  float32 = 24
+
+	playerSpeed   float32 = 3
+	maxKeyDownCnt int     = 10
+)
+
+func load() {
+	penguinSprite = rl.LoadTexture("sprites/penguin.png")
+	spriteRect = rl.NewRectangle(0, spriteFaceDir*spriteHeight, 24, 32)
+	playerRec = rl.NewRectangle(300, 300, 48, 64)
+}
+
+func unload() {
+	rl.UnloadTexture(penguinSprite)
+}
+
+func getInput() {
+	keyDownCnt++
+
+	switch {
+	case rl.IsKeyDown(rl.KeyW):
+		spriteRect.Y = faceUp * spriteHeight
+		playerRec.Y -= playerSpeed
+	case rl.IsKeyDown(rl.KeyA):
+		spriteRect.Y = faceLeft * spriteHeight
+		playerRec.X -= playerSpeed
+	case rl.IsKeyDown(rl.KeyS):
+		spriteRect.Y = faceDown * spriteHeight
+		playerRec.Y += playerSpeed
+	case rl.IsKeyDown(rl.KeyD):
+		spriteRect.Y = faceRight * spriteHeight
+		playerRec.X += playerSpeed
+	default:
+		keyDownCnt--
+	}
+
+	if keyDownCnt == maxKeyDownCnt {
+		spriteSeq = (spriteSeq + 1) & 3
+		spriteRect.X = float32(spriteSeq) * spriteWidth
+		keyDownCnt = 0
+	}
+
+}
+
+func drawScene() {
+	rl.DrawTexturePro(penguinSprite, spriteRect, playerRec, rl.NewVector2(playerRec.Width, playerRec.Height), 0, rl.White)
+}
+
 func main() {
-	rl.InitWindow(300, 300, "Hello, World!")
+	rl.InitWindow(600, 600, "Hello, World!")
+	rl.SetExitKey(0)
 	rl.SetTargetFPS(60)
 
-	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
+	load()
 
+	for !rl.WindowShouldClose() {
+		getInput()
+
+		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Hello, World!", 0, 0, 20, rl.LightGray)
+
+		drawScene()
 
 		rl.EndDrawing()
 	}
+
+	unload()
 
 	rl.CloseWindow()
 }
